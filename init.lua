72532e3cfcefdb932572eab26352664da7b3f0a6
Jurgis Bridžius <jurgis@bridzius.lt>
2024-01-09 22:26:58 +0200

    finally fully lua'd the config Plugins are also finally portable and should work across different installs

--- init.lua
+++ init.lua
@@ -4,28 +4,94 @@ vim.g.loaded_netrwPlugin = 1
 
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.textwidth = 80
+vim.opt.colorcolumn = '80'
+vim.opt.whichwrap:append('<,>,h,l')
+vim.keymap.set('n', 'W', 'w');
+vim.keymap.set('n', 'Wq', 'wq');
+vim.keymap.set('n', 'Q', 'q');
+vim.keymap.set('n', 'wQ', 'wq');
+vim.keymap.set('n', 'WQ', 'wq');
+vim.keymap.set('n', 'qw', 'wq');
 
-require("plugins");
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.cmd([[
-set number
-" colorscheme
-syntax on
-" colorscheme gruvbox
-filetype on
-filetype plugin indent on
-" only allow 80 char width and add a column on 80 chars to illuminate.
-set textwidth=80
-set colorcolumn=80
-" make navigation keys navigate to next line on line-ends
-set whichwrap+=<,>,h,l
-cnoreabbrev W w
-cnoreabbrev Wq wq
-cnoreabbrev Q q
-cnoreabbrev wQ wq
-cnoreabbrev WQ wq
-cnoreabbrev qw wq
-]])
+require("lazy").setup("plugins")
 
--- empty setup using defaults
-require("nvim-tree").setup()
+-- Set updatetime to 3s for better UX.
+vim.opt.updatetime = 300
+
+-- Always show signcolumn.
+vim.opt.signcolumn = "yes"
+
+local keyset = vim.keymap.set
+-- Autocomplete
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Highlight the symbol and its references on a CursorHold event(cursor is idle)
+vim.api.nvim_create_augroup("CocGroup", {})
+vim.api.nvim_create_autocmd("CursorHold", {
+    group = "CocGroup",
+    command = "silent call CocActionAsync('highlight')",
+    desc = "Highlight symbol under cursor on CursorHold"
+})
+
+-- Formatting selected code
+keyset("x", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+keyset("n", "<leader>f", "<Plug>(coc-format-selected)", {silent = true})
+
+-- Remap keys for apply refactor code actions.
+keyset("n", "<leader>re", "<Plug>(coc-codeaction-refactor)", { silent = true })
+keyset("x", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+keyset("n", "<leader>r", "<Plug>(coc-codeaction-refactor-selected)", { silent = true })
+
+-- Run the Code Lens actions on the current line
+keyset("n", "<leader>cl", "<Plug>(coc-codelens-action)", opts)
+
+-- Add `:Format` command to format current buffer
+vim.api.nvim_create_user_command("Format", "call CocAction('format')", {})
+
+-- Add `:OR` command for organize imports of the current buffer
+vim.api.nvim_create_user_command("OR", "call CocActionAsync('runCommand', 'editor.action.organizeImport')", {})
+
+-- Add (Neo)Vim's native statusline support
+-- NOTE: Please see `:h coc-status` for integrations with external plugins that
+-- provide custom statusline: lightline.vim, vim-airline
+vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}")
+
+vim.opt.foldmethod     = 'expr'
+vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+---WORKAROUND
+vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod     = 'expr'
+    vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+  end
+})
+---ENDWORKAROUND
+
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,51 +1,83 @@
-
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
--- https://github.com/wbthomason/packer.nvim
-	use'leafgarland/typescript-vim'
--- https://github.com/leafgarland/typescript-vim
-	use {
+return {
+	{
+		'folke/tokyonight.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	-- https://github.com/folke/tokyonight.nvim
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' }
+	},
+	-- https://github.com/nvim-lualine/lualine.nvim
+	{
+		'nvimdev/dashboard-nvim',
+		event = 'VimEnter',
+		config = function()
+			require('dashboard').setup({
+				shortcut_type = 'number',
+				hide = {
+					statusline = false
+				}
+			})
+		end,
+		dependencies = { {'nvim-tree/nvim-web-devicons'}}
+	},
+	-- https://github.com/nvimdev/dashboard-nvim
+	'leafgarland/typescript-vim',
+	-- https://github.com/leafgarland/typescript-vim
+	{ 
 		'neoclide/coc.nvim', 
 		branch = 'release' 
-	}
--- https://github.com/neoclide/coc.nvim
-	use 'morhetz/gruvbox'
--- https://github.com/morhetz/gruvbox
-	use 'neovim/nvim-lspconfig'
--- https://github.com/neovim/nvim-lspconfig
-	use {
-  		'nvim-tree/nvim-tree.lua',
-  		requires = {
-    			'nvim-tree/nvim-web-devicons', -- optional
-  		},
-  		config = function()
-    			require("nvim-tree").setup {}
-  		end
-	}
--- https://github.com/nvim-tree/nvim-tree.lua"
-	use 'tpope/vim-commentary'
--- https://github.com/tpope/vim-commentary
-	use {
-		'junegunn/fzf', 
-		run = 'fzf install'
-	}
--- https://github.com/junegunn/fzf
-	use 'junegunn/fzf.vim'
--- https://github.com/junegunn/fzf.vim
-	if packer_bootstrap then
-		require('packer').sync()
-	end
-end)
+	},
+	-- https://github.com/neoclide/coc.nvim
+	{ 
+		'nvim-treesitter/nvim-treesitter', 
+		build = function() 
+			require("nvim-treesitter.install").update({ with_sync = true })() 
+		end,
+	},
+	{
+		'nvim-treesitter/nvim-treesitter',
+		opts = {
+			ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query'},
+			sync_install = true,
+			ignore_install = { 'javascript' },
+			highlight = {
+				enable = true,
+				disable = { 'c' },
+				disable = function(lang, buff)
+					local max_filesize = 100 * 1024
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+				additional_vim_regex_highlighting = false
+			},
+			autotag = {
+				enable = true
+			}
+		}
+	},
+	-- https://github.com/nvim-treesitter/nvim-treesitter
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+    		"telescope.nvim",
+    		dependencies = {
+      			"nvim-telescope/telescope-fzf-native.nvim",
+      			build = "make",
+      			config = function()
+        			require("telescope").load_extension("fzf")
+      			end,
+    		}
+  	}
+	-- https://github.com/nvim-telescope/telescope.nvim
+}
